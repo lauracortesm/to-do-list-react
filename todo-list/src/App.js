f65258126fe9940c093b92c1225f7ce24ae29811
lauracortesm <laura.cortem96@gmail.com>
2021-07-30 23:41:04 -0500

    Todo List

---
+++ todo-list/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoForm from './Components/TodoForm/TodoForm';
+import TodoList from './Components/TodoList/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+
+    </div>
+    
+  );
+}
+
+export default App;
